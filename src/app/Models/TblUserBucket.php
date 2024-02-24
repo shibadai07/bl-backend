42caad63fcb3362a672d14f7f6fe6806521484ba
shibadai <shiba071707@gmail.com>
2024-02-24 10:31:03 +0900

    no message

---
+++ src/app/Models/TblUserBucket.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TblUserBucket extends Model
+{
+    use HasFactory;
+    protected $table = 'tbl_user_bucket';
+    
+    public function addMyBucket(){
+        $tbl_user_bucket = new TblUserBucket;
+        $tbl_user_bucket->user_id = $request['user_id'];
+        $tbl_user_bucket->bucket_bucket_id = $request['bucket_id'];
+        $tbl_user_bucket->status_id = $request['status_id'];
+        $tbl_user_bucket->create_date = now()->toDateTimeString();
+        $tbl_user_bucket->update_date = now()->toDateTimeString();
+        $tbl_user_bucket->timestamps = false;
+        $tbl_user_bucket->save();
+    }
+
+
+}
